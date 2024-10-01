0474d1b34e890a02e99fbfde64b199312784f6a0
An Phạm <an.pd01114@sinhvien.hoasen.edu.vn>
2024-10-01 16:38:20 +0700

    An

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 
 # tạo ra được 1 trường input
 # tạo ra được 2 button (button: vẽ graph, random graph)
@@ -27,8 +28,31 @@ def split_input():
 # mỗi khi random được 1 cạnh thì random 1 số weight nhất định
 
 # result: số đỉnh và số cạnh của mỗi đỉnh
-def random_graph():
-    return None
+def random_graph(max_num_nodes_input,max_num_edges_input):
+    num_nodes = []
+    num_nodes_random = random.randint(2, max_num_nodes_input)
+    for i in range(num_nodes_random):
+        num_nodes.append(i + 1)
+
+    max_num_edges_of_node = len(num_nodes) - 1
+    min_num_edges_of_node = 1
+
+    if (max_num_edges_input > max_num_edges_of_node) or (max_num_edges_input < min_num_edges_of_node):
+        print("Vui long nhap lai: ")
+
+    print(num_nodes)
+    for i in range(len(num_nodes)):
+        num_edges = random.randint(min_num_edges_of_node, max_num_edges_of_node)
+        num_nodes_expected = num_nodes.copy()
+        num_nodes_expected.remove(num_nodes[i])
+        isUndirected = random.randint(0, 1)
+        # dô thi vo huong
+        if isUndirected:
+            num_nodes_expected.remove(num_nodes[i])
+
+        for j in range(len(num_nodes_expected) - num_edges):
+            num_nodes_expected.remove(random.choice(num_nodes_expected))
+        print(f"dinh {num_nodes[i]} co ${num_edges} canh: ${num_nodes_expected}")
 
 
 def draw_graph():
